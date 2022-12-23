fd1cd21febbaf185ed5586db494ccdea95717502
Mayurebiztrait <114912976+Mayurebiztrait@users.noreply.github.com>
2022-12-23 18:44:48 +0530

    category listing changes

--- template/category-listing.php
+++ template/category-listing.php
@@ -14,22 +14,21 @@ get_header(); ?>
         <div class="container-xxxl container-xxl container">
             <?php
             $args = array(
-                'product_cat' => $category->slug,
-                'posts_per_page' => -1,
-                'post_status' => 'publish',
+                'taxonomy' => 'product_cat',
                 'orderby' => 'name',
                 'order' => 'ASC',
+                'hide_empty' => true
             );
             foreach( get_categories( $args ) as $category ) { ?>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="product-details-title">
-                        <h3><?php echo $category->name;?></h3>
-                        <p><?php echo $category->category_description;?></p>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="product-details-title">
+                            <h3><?php echo $category->name;?></h3>
+                            <p><?php echo $category->category_description;?></p>
+                        </div>
                     </div>
-                </div>
             
-                <?php
+                    <?php
                     $args = array(
                         'product_cat' => $category->slug,
                         'posts_per_page' => -1,
@@ -39,39 +38,29 @@ get_header(); ?>
                     );
                     $loop = new WP_Query($args);
                     while ($loop->have_posts()) : $loop->the_post();
-                        global $product;
-                        $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->id ), 'single-post-thumbnail' );
-                        ?>
-                        <div class="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                            <div class="product-details-wrapper">
-                                <a href="<?php echo site_url('/simple-product-detail').'?productid='.$product->id;?>">
-                                    <div class="product-details-main-img">
-                                        <img src="<?php echo $image[0];?>" alt="<?php the_title(); ?>" >
-                                    </div>
-                                    <div class="product-details-content">
-                                        <h2><?php the_title(); ?></h2>
-                                        <h3>Starting at <span>$<?php echo $product->price; ?></span> us MSRP</h3>
-                                        <p>
-                                            <?php echo $product->short_description; ?>
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
+                    global $product;
+                    $image = wp_get_attachment_image_src( get_post_thumbnail_id( $product->id ), 'single-post-thumbnail' );
+                    ?>
+                    <div class="col-12 col-sm-12 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                        <div class="product-details-wrapper">
+                            <a href="<?php echo site_url('/simple-product-detail').'?productid='.$product->id;?>">
+                                <div class="product-details-main-img">
+                                    <img src="<?php echo $image[0];?>" alt="<?php the_title(); ?>" >
+                                </div>
+                                <div class="product-details-content">
+                                    <h2><?php the_title(); ?></h2>
+                                    <h3>Starting at <span>$<?php echo $product->price; ?></span> us MSRP</h3>
+                                    <p>
+                                        <?php echo $product->short_description; ?>
+                                    </p>
+                                </div>
+                            </a>
                         </div>
-                        <div class="product-details-content">
-                            <h2><?php the_title(); ?></h2>
-                            <h3>Starting at <span>$<?php echo $product->price; ?></span> us MSRP</h3>
-                            <p>
-                                <?php echo $product->short_description; ?>
-                            </p>
-                        </div>
-                    </a>
+                    </div>
+                    <?php endwhile; ?>
+                    <?php wp_reset_query(); ?>
+            
                 </div>
-            </div>
-
-            <?php endwhile; ?>
-            <?php wp_reset_query(); ?>
-        </div>
         <?php } ?>
 
     </div>
