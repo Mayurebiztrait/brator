499d61530e77dc94fe40012b3de101631ee4b551
Puneetebiztrait <Puneet.ebiztrait@gmail.com>
2022-12-26 14:22:44 +0530

    catalog

--- taxonomy-catalogs_categories.php
+++ taxonomy-catalogs_categories.php
@@ -63,17 +63,18 @@ margin-bottom: 14px;
 }
 </style>
 <div class="top-banner">
-<div class="row section-1">
+<div class="inner-top">
+<div class="row section-top-1">
 <div class="col-sm-2"></div>
 <div class="col-sm-8"><h3>AODES</h3></div>
 <div class="col-sm-2"></div>
 </div>
-<div class="row section-2">
+<div class="row section-top-2">
 <div class="col-sm-2"></div>
 <div class="col-sm-8"><p>CATALOGS & <?php single_term_title();?></p></div>
 <div class="col-sm-2"></div>
 </div>
-
+</div>
 </div>
 </div>
 <!--===================================-->
