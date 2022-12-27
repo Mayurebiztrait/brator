96fb85ce7dcec055c578795275479b0f0f985545
Puneetebiztrait <Puneet.ebiztrait@gmail.com>
2022-12-27 13:08:46 +0530

    function

--- functions.php
+++ functions.php
@@ -904,3 +904,67 @@ function special_nav_class ($classes, $item) {
   }
   return $classes;
 }
+// 27 dec 
+// Custom Care manuals Custom Post Type:
+function custom_post_type_custom_care_manuals() {
+  
+// Set UI labels for Custom Post Type
+    $labels = array(
+        'name'                => _x( 'Manuals', 'Post Type General Name', 'twentytwentyone' ),
+        'singular_name'       => _x( 'Manual', 'Post Type Singular Name', 'twentytwentyone' ),
+        'menu_name'           => __( 'Manuals', 'twentytwentyone' ),
+        'parent_item_colon'   => __( 'Parent Manual', 'twentytwentyone' ),
+        'all_items'           => __( 'All Manuals', 'twentytwentyone' ),
+        'view_item'           => __( 'View Manual', 'twentytwentyone' ),
+        'add_new_item'        => __( 'Add New Manual', 'twentytwentyone' ),
+        'add_new'             => __( 'Add New', 'twentytwentyone' ),
+        'edit_item'           => __( 'Edit Manual', 'twentytwentyone' ),
+        'update_item'         => __( 'Update Manual', 'twentytwentyone' ),
+        'search_items'        => __( 'Search Manual', 'twentytwentyone' ),
+        'not_found'           => __( 'Not Found', 'twentytwentyone' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'twentytwentyone' ),
+    );
+      
+// Set other options for Custom Post Type
+      
+    $args = array(
+        'label'               => __( 'manuals', 'twentytwentyone' ),
+        'description'         => __( 'Manual news and reviews', 'twentytwentyone' ),
+        'labels'              => $labels,
+        // Features this CPT supports in Post Editor
+        'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
+        // You can associate this CPT with a taxonomy or custom taxonomy. 
+        'taxonomies'          => array( 'genres' ),
+        /* A hierarchical CPT is like Pages and can have
+        * Parent and child items. A non-hierarchical CPT
+        * is like Posts.
+        */
+        'hierarchical'        => false,
+        'public'              => true,
+        'show_ui'             => true,
+        'show_in_menu'        => true,
+        'show_in_nav_menus'   => true,
+        'show_in_admin_bar'   => true,
+        'menu_position'       => 5,
+        'can_export'          => true,
+        'has_archive'         => true,
+        'exclude_from_search' => false,
+        'publicly_queryable'  => true,
+        'capability_type'     => 'post',
+        'show_in_rest' => true,
+			
+		'taxonomies'  => array( 'category' ),
+  
+    );
+      
+    // Registering your Custom Post Type
+    register_post_type( 'manuals', $args );
+  
+}
+  
+/* Hook into the 'init' action so that the function
+* Containing our post type registration is not 
+* unnecessarily executed. 
+*/
+  
+add_action( 'init', 'custom_post_type_custom_care_manuals', 0 );
