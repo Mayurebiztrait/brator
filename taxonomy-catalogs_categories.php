af021a017ef22b94f3987de556023ea75fdb1e03
Mayurebiztrait <114912976+Mayurebiztrait@users.noreply.github.com>
2022-12-30 11:04:01 +0530

    ketul changes live to github

--- taxonomy-catalogs_categories.php
+++ taxonomy-catalogs_categories.php
@@ -14,105 +14,89 @@
 
 get_header();
 
-if ( is_active_sidebar( 'sidebar-1' ) ) :
-	$blog_post_list_class = 'col-xl-9';
-else :
-	$blog_post_list_class = 'col-xl-12';
+if (is_active_sidebar("sidebar-1")):
+    $blog_post_list_class = "col-xl-9";
+else:
+    $blog_post_list_class = "col-xl-12";
 endif;
 ?>
-<style>
-.top-banner {
-    padding-top: 425px;
-    background-image: url(https://aodes.solutiontrackers.biz/wp-content/uploads/2022/12/download.png)!important;
-    background-size: cover;
-}
-</style>
+
 <div class="top-banner">
-<div class="inner-top">
-<div class="row section-top-1">
-<div class="col-sm-2"></div>
-<div class="col-sm-8"><h3>AODES</h3></div>
-<div class="col-sm-2"></div>
-</div>
-<div class="row section-top-2">
-<div class="col-sm-2"></div>
-<div class="col-sm-8"><p>CATALOGS & <?php single_term_title();?></p></div>
-<div class="col-sm-2"></div>
-</div>
-</div>
+    <div class="inner-top">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>AODES</h3>
+                    <p>CATALOGS & <?php single_term_title();?></p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 </div>
 <!--===================================-->
 <div class="top-middle-banner">
-<div class="inner-section">
-<div class="row section-1">
-<div class="col-sm-4"></div>
-<div class="col-sm-4 ct"><h3>INCLUDE EVERYTHING REGARDING IN 2023 AODES LINEUP.</h3></div>
-<div class="col-sm-4"></div>
-</div>
-<div class="row section-2">
-<div class="col-sm-2"></div>
-<div class="col-sm-8"><p>VEHICLES, ACCESSORIES & GEAR!</p></div>
-<div class="col-sm-2"></div>
-</div>
-
-</div>
+    <div class="inner-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="top-middle-banner-wrapper">
+                        <h3>INCLUDE EVERYTHING REGARDING IN 2023 AODES LINEUP.</h3>
+                        <p>VEHICLES, ACCESSORIES & GEAR!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <div class="brator-blog-post-area catalogs-archive">
-	<div class="container-xxxl container-xxl container">
-		<div class="row main-title-catalog">
-			<div class="col-lg-12">
-				<div class="brator-breadcrumb">
-					<h1 class="text-center">Download <span><?php single_term_title();?> Catalog</span></h1>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="container-xxxl container-xxl container">
-		
-				<div class="brator-blog-post bottom-catalog">
-					<?php
-					if ( have_posts() ) : ?>
-                        <div class="row">
-                        <?php 
-                            while ( have_posts() ) :
-                                the_post();
-                                ?>
-                                
-                                <div class="col-md-3 col-sm-6 col-12 mb-4">
-                                    <?php
-                                    get_template_part( 'template-parts/blog-layout/blog-catalog-content' );
-                                ?>
-                                </div> 
-                    
-                            
-                            <?php
-                            endwhile;
-                        else :
-                            get_template_part( 'template-parts/content', 'none' );
-                        endif; ?>
-                    </div>
-                    <?php
-					wp_reset_postdata();
-					?>
-					<?php if ( get_the_posts_pagination() ) : ?>
-						<div class="brator-pagination-box ">
-							<?php
-								the_posts_pagination(
-									array(
-										'mid_size'  => 2,
-										'prev_text' => '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+    <div class="container-xxxl container-xxl container">
+        <div class="row main-title-catalog">
+            <div class="col-lg-12">
+                <div class="brator-breadcrumb">
+                    <h1 class="text-center">Download <span><?php single_term_title();?> Catalog</span></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-xxxl container-xxl container">
+
+        <div class="brator-blog-post bottom-catalog">
+            <?php if (have_posts()): ?>
+            <div class="row">
+                <?php while (have_posts()):
+    the_post();?>
+
+                <div class="col-md-3 col-sm-6 col-12 mb-4">
+                    <?php get_template_part(
+        "template-parts/blog-layout/blog-catalog-content"
+    );?>
+                </div>
+
+
+                <?php
+endwhile;else:get_template_part(
+        "template-parts/content",
+        "none"
+    );endif;?>
+            </div>
+            <?php wp_reset_postdata();?>
+            <?php if (get_the_posts_pagination()): ?>
+            <div class="brator-pagination-box ">
+                <?php the_posts_pagination([
+    "mid_size" => 2,
+    "prev_text" =>
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
 										<path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
-									  </svg>' . esc_html__( 'Prev', 'brator' ),
-										'next_text' => esc_html__( 'Next', 'brator' ) . '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path></svg>',
-									)
-								);
-							?>
-						</div>
-					<?php endif; ?>
-				</div>
-			
-	</div>
+									  </svg>' . esc_html__("Prev", "brator"),
+    "next_text" =>
+    esc_html__("Next", "brator") .
+    '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path></svg>',
+]);?>
+            </div>
+            <?php endif;?>
+        </div>
+
+    </div>
 </div>
-<?php
-get_footer();
+<?php get_footer();
